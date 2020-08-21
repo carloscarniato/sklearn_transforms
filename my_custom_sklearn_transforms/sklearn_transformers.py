964dd85aadd5d07bbb288efd8d67379a64eadb80
carloscarniato <35527224+carloscarniato@users.noreply.github.com>
2020-08-21 18:43:11 -0300

    Update sklearn_transformers.py
    correction

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -16,12 +16,11 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
     
-class OneHot(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
+class CreateDummies(BaseEstimator, TransformerMixin):
+
+    def transform(self, X, **transformparams):
+        dum = pd.get_dummies(X).copy()
+        return pd.concat([df, dum], axis=1) 
+
+    def fit(self, X, y=None, **fitparams):
         return self
-    
-    def transform(self, X):
-        data = X.copy()
-        data.loc[:,'PERFIL_DUMMY'] = data['PERFIL']
-        dum_df = pd.get_dummies(data, columns=["PERFIL_DUMMY"], prefix=['PERFIL'])# merge with main df bridge_df on key values
-        return dum_df
